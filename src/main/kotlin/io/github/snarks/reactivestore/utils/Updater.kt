722160403750d4937285c4d98eb7e260a6ddfde0
James Cruz <snarks@users.noreply.github.com>
2018-02-09 13:35:44 +0800

    Update documentation on Updater.kt
    This clarified how Updaters can be executed when it is used.
    
    This also expanded the requirements for an Updater:
    "stateless" -> "(preferably) stateless or immutable"

--- src/main/kotlin/io/github/snarks/reactivestore/utils/Updater.kt
+++ src/main/kotlin/io/github/snarks/reactivestore/utils/Updater.kt
@@ -8,6 +8,7 @@ import io.reactivex.Single
  *
  * If the return value of this function is the same as `currentStatus`, it means no change should happen in the store.
  *
- * Updaters **should be stateless** since they can be called in concurrent contexts.
+ * Updaters can be invoked multiple times and may be called on different threads with no particular order.
+ * So preferably, updaters should be **stateless or immutable**.
  */
 typealias Updater<T> = (currentStatus: LoadStatus<T>, defaultLoader: Single<out T>) -> LoadStatus<T>
